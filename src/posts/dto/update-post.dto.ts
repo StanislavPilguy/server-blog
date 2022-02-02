1bb50f9e5bee008e92609d480020e4c1640b7cb4
Stanislav Pilguy <stanislav.pilguy@gmail.com>
2022-02-02 17:05:02 +0200

    added post

--- src/posts/dto/update-post.dto.ts
+++ src/posts/dto/update-post.dto.ts
@@ -1,8 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class UpdatePostDto {
+  @ApiProperty({ example: '1', description: 'Unique Identificator' })
   readonly id: number;
+  @ApiProperty({ example: 'Title', description: 'Post title' })
   readonly title: string;
+  @ApiProperty({ example: 'Description', description: 'Post description' })
   readonly description: string;
+  @ApiProperty({ example: 'Content', description: 'Post content' })
   readonly content: string;
+
   readonly userId: number;
+  @ApiProperty({ example: 'Image', description: 'Post images' })
   readonly image: string;
 }
