9b6a02db2526809cebd18f513eb68898efbdb6be
Stanislav <stanislav.pilguy@gmail.com>
2022-05-27 12:30:44 +0300

    change user service method create in role 0

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -23,8 +23,6 @@ export class CategoriesController {
   constructor(private categoriesService: CategoriesService) {}
   @ApiOperation({ summary: 'Create category' })
   @ApiResponse({ status: 200, type: Category })
-  @Roles('1', '2')
-  @UseGuards(RolesGuard)
   @Post()
   createCategory(@Body() createCategory: CreateCategoryDto) {
     return this.categoriesService.create(createCategory);
